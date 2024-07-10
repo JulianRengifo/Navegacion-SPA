7aafc98be8c9e8da42c363bdef36fe72a2f9da67
JulianRengifo <jurenco1997@gmail.com>
2024-07-09 23:32:53 -0500

    React Router Dom
    Registar rutas
    
    En App.js importamos BrowserRouter
    
    Le indicamos la rutas que necesitamos en react-router-dom, agragamos Routes y Route
    
    Route hace referencia a la ruta
    
    En Route se agrega path que es el camino, que empieza en la pagina principal /
    
    Luego se agrega element que es el elemento que se quiere mostar, en este caso es Home
    
    De igual forma agregamos el elemento Sobre en Route

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
 
-  //Nos ayuda a mirar la información sobre la ubicación actual del navegador
-  /*lo que nos interesa ver es la ubicacion a la que nos lleva al darle click en una opcion,
-  para eso miramos pathname: La parte de la URL después del nombre de host.*/
-
-  const location = window.location.pathname
-  console.log(location)
-
-  const Router = () => {
-    if(location === "/"){
-      return <Home/>
-  }else{
-    return <Sobre/>
-  }
-}
+  
 
   return (
     <>
-      {Router()}
+      <BrowserRouter>
+        <Routes>
+          {/*Route hace referencia a la ruta*/}
+          {/*se agrega path que es el camino, que empieza en la pagina principal "/" */}
+          {/*Luego se agrega element que es el elemento que se quiere mostar, en este caso es Home*/}
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Sobre' element={<Sobre/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
