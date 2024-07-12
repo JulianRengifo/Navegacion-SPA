b352cd452e933a5552b59f8be79d0d49ea536c99
JulianRengifo <jurenco1997@gmail.com>
2024-07-12 00:26:45 -0500

    Creando navbar
    Creamos una carpeta llamada components
    
    En la carpeta components, creamos Header.jsx, este sera un dashboard
    
    En Header.jsx importamos la imagen y el css de Header, creamos el componente Header con su estructura
    
    En App.js importamos Header y llamamos el componente Header pero por fuera de las rutas para que este pueda ser visible en cada ruta

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 import Page404 from './pages/Pages404';
+import Header from './components/Header';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
@@ -13,7 +15,10 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      {/*Lo que se coloca en BrowserRouter se comparte con las demas rutas*/}
+      <Header/>
         <Routes>
+          {/*Lo que esta dentro de Routes va a ser dinamico*/}
           {/*Route hace referencia a la ruta*/}
           {/*se agrega path que es el camino, que empieza en la pagina principal "/" */}
           {/*Luego se agrega element que es el elemento que se quiere mostar, en este caso es Home*/}
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import image from "../assets/img/doguito.svg"
+import "../assets/css/componentes/header.css"
+
+const Header = () => {
+    return(
+        <header className="header container">
+            <div className="menu-hamburguer">
+                <span className="menu-hamburguer__icon"/>
+            </div>
+            <div className="header-containerr">
+                <a href="/" className="flex flex-center">
+                    <img className="header__logo" src={image} alt="doguito"/>
+                    <h1 className="header__title">Petshop</h1>
+                </a>
+            </div>
+
+            <nav className="menu-header">
+                <ul className="menu-items">
+                    <li><a className="meu-item menu-item__entrar" href="#">Entar</a></li>
+                    <li><a className="menu-item" href="#">Productos</a></li>
+                    <li><a className="menu-item" href="/">Blog</a></li>
+                    <li><a className="menu-item" href="/sobre">Sobre</a></li>
+                </ul>
+            </nav>
+            <div className="menu-header-background"></div>
+        </header>
+    )
+}
+
+export default Header
