eef1a9e1326144ef8fea5a7692b67de6fa9b360e
JulianRengifo <jurenco1997@gmail.com>
2024-07-16 00:49:27 -0500

    Mostrar los posts
    En Home.jsx importamos ListPosts
    
    Agregamos el componente LisPosts dondes se pasa la prop url y el valor /posts
    
    En LisPosts importamos la funcion buscar
    
    Se necesita conectar al servidor y poder actualizar la informacion, para eso se importa useState y useEffect
    
    Se crea una constante con la variable post y una funcion para actualizarla
    
    Se usa useEffect para buscar la informacion
    
    En LisPosts  necesitamos recorrer el arreglo de posts
    
    Destructuramos post, extrayendo los datos de cada post
    
    Crea un enlace (<Link>) con una ruta dinámica basada en el ID del post y aplica clases CSS dinámicas

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -1,10 +1,50 @@
+import { useState, useEffect } from "react"
 import "../assets/css/componentes/card.css"
+import { buscar } from "../api/api"
+import { Link } from "react-router-dom"
 
 // recibimos la url
 const ListPosts = ({url}) => {
+    
+    //useState es un hook de React que permite agregar estado a un componente funcional.
+    //Se declara una variable de estado llamada post y una función para actualizarla llamada setPosts.
+    //El estado inicial de post es un array vacío [].
+    const [posts, setPosts] = useState ([])
+
+    /*useEffect es otro hook de React que se usa para manejar efectos secundarios en componentes funcionales.
+    Este hook se ejecuta después de que el componente se renderiza por primera vez y cada vez que cambia 
+    una de las dependencias especificadas en el array de dependencias (en este caso, url).
+    Se usa useEffect para buscar la informacion*/
+    useEffect(() => {
+        //hace una solicitud a la API para obtener datos y luego actualiza el estado post utilizando setPosts
+        buscar(url, setPosts)
+    }, [url]) // Cada vez que cambie la url se ejecute esta funcion
 
     return(
-        <section className="post container"></section>
+        <section className="posts container">
+            {
+                // Tenemos un arrglo y usamos map para recorrelo uno por uno
+                posts.map(post => {
+                    // Destructuramos cada post obteniendo id, title...
+                    const {id, title, metadescription, categoria} = post;
+                    // /posts/${id}` nos agrega un identificador a cada posts y que al seleccionar una card nos muestre el detalle de esa card
+                    // post-card--${categoria} modifica el comportamiento de la categoria, si la card es roja o azul
+                    // La propiedad key es necesaria en listas generadas dinámicamente para ayudar a React a identificar y gestionar cada elemento de manera eficiente.
+                    return <Link to={`/posts/${id}`} className={`post__card post-card--${categoria}`} key={id}>
+                        <article>
+                            <h3 className="post-card__title">
+                                {/*muestra el título del post*/}
+                                {title}
+                            </h3>
+                            <p className="post-card__meta">
+                                {/*muestra la metadescripción del post*/}
+                                {metadescription}
+                                </p>
+                        </article>
+                    </Link>
+                })
+            }
+        </section>
     )
 }
 
